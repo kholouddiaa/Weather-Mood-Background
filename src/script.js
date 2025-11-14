ae20b8231fd5ef6a4738b9c47880f2cd49c9cb5d
kholouddiaa <kholoud.diaa@el-eng.menofia.edu.eg>
2025-11-14 10:57:47 +0200

    first commit from local project

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let btnWeatherInput = document.getElementById("weatherInput"); //get input text
 let btnChangeWeather = document.getElementById("changeBtn");
 let message = document.getElementById("message");
@@ -40,3 +41,61 @@ btnChangeWeather.onclick = function () {
 
 
 
+=======
+//get input textbox, button, and message elements
+const weatherInput = document.getElementById("weatherInput");
+const btnChangeWeather = document.getElementById("changeBtn");
+const message = document.getElementById("message");
+const sunny = "url('https://images.pexels.com/photos/1198507/pexels-photo-1198507.jpeg')";
+const rainy = "url('https://images.pexels.com/photos/1250672/pexels-photo-1250672.jpeg')";
+const cloudy = "url('https://images.pexels.com/photos/7919/pexels-photo.jpg')";
+
+const arr = [rainy, sunny, cloudy];
+//Allow Enter key to trigger button click
+weatherInput.addEventListener("keyup", e => {
+    if (e.key === "Enter") btnChangeWeather.click();
+});
+//Change background image based on input
+btnChangeWeather.addEventListener("click", function () {
+    //Convert input to lowercase and trim whitespace
+    const value = weatherInput.value.toLowerCase().trim();
+    if (!value) {
+        message.innerText = "Please type something!";
+        return;
+    }
+
+    // convert input (value) to index in arr
+    let index;
+
+    if (value === "rainy") {
+        index = 0;
+    } else if (value === "sunny") {
+        index = 1;
+    } else if (value === "cloudy") {
+        index = 2;
+    }
+
+
+
+    //Check if input is valid
+    switch (index) {
+        case 0://rainy
+            document.body.style.backgroundImage = arr[0];
+            message.innerText = "Don't forget your umbrella!"
+            break;
+        case 1: //sunny
+            document.body.style.backgroundImage = arr[1];
+            message.innerText = "It's a bright and sunny day!"
+            break;
+        case 2: //cloudy
+            document.body.style.backgroundImage = arr[2];
+            message.innerText = "A calm and cloudy day";
+            break;
+        default:
+            document.body.style.backgroundImage = "";
+            message.innerText = 'Please enter a "sunny", "rainy", or "cloudy"';
+    }
+
+    weatherInput.value = "";
+});
+>>>>>>> 9b3016c (first commit from local project)
